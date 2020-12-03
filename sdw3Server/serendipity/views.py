258c80c5ad0ac4d0f3748735af71f592ec84b5d9
Davisa94 <Austin.i.davis94@gmail.com>
2020-12-03 14:21:57 -0700

    deleted all but one view at one method

--- sdw3Server/serendipity/views.py
+++ sdw3Server/serendipity/views.py
@@ -25,15 +25,3 @@ def course_list(request):
         return JsonResponse(serendipity_serializer.data, safe=False)
         # 'safe=False' for objects serialization
  
-    elif request.method == 'POST':
-        tutorial_data = JSONParser().parse(request)
-        tutorial_serializer = TutorialSerializer(data=tutorial_data)
-        if tutorial_serializer.is_valid():
-            tutorial_serializer.save()
-            return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        count = Tutorial.objects.all().delete()
-        return JsonResponse({'message': '{} Tutorials were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
- 
